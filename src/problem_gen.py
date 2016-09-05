b216a36b17b7ee8f5c1399a327b6f67a52d8bced
Roy Smart <roytsmart@gmail.com>
2016-09-05 12:14:14 -0600

    problem generator still has rare error

--- src/problem_gen.py
+++ src/problem_gen.py
@@ -16,28 +16,41 @@ class graph_edge():
 # Solution given by http://stackoverflow.com/a/1968345
 def does_cross(line1, line2):
 
-    p0_x = line1.getP1().getX()
-    p0_y = line1.getP1().getY()
-    p1_x = line1.getP2().getX()
-    p1_y = line1.getP2().getY()
-    p2_x = line2.getP1().getX()
-    p2_y = line2.getP1().getY()
-    p3_x = line2.getP2().getX()
-    p3_y = line2.getP2().getY()
+    try:
+        p0_x = line1.getP1().getX()
+        p0_y = line1.getP1().getY()
+        p1_x = line1.getP2().getX()
+        p1_y = line1.getP2().getY()
+        p2_x = line2.getP1().getX()
+        p2_y = line2.getP1().getY()
+        p3_x = line2.getP2().getX()
+        p3_y = line2.getP2().getY()
 
-    s1_x = p1_x - p0_x
-    s1_y = p1_y - p0_y
-    s2_x = p3_x - p2_x
-    s2_y = p3_y - p2_y
+        s1_x = p1_x - p0_x
+        s1_y = p1_y - p0_y
+        s2_x = p3_x - p2_x
+        s2_y = p3_y - p2_y
 
-    s = (-s1_y * (p0_x - p2_x) + s1_x * (p0_y - p2_y)) / (-s2_x * s1_y + s1_x * s2_y)
-    t = (s2_x * (p0_y - p2_y) - s2_y * (p0_x - p2_x)) / (-s2_x * s1_y + s1_x * s2_y)
+        s = (-s1_y * (p0_x - p2_x) + s1_x * (p0_y - p2_y)) / (-s2_x * s1_y + s1_x * s2_y)
+        t = (s2_x * (p0_y - p2_y) - s2_y * (p0_x - p2_x)) / (-s2_x * s1_y + s1_x * s2_y)
 
-    print(s,t)
-    if (s > 0 and s < 1 and t > 0 and t < 1):
+        i_x = p0_x + (t * s1_x)
+        i_y = p0_y + (t * s1_y)
+
+        print(s,t)
+        if (s >= 0 and s <= 1 and t >= 0 and t <= 1):
+            if (i_x != p0_x and i_x != p1_x and i_x != p2_x and i_x != p3_x):
+                if (i_y != p0_y and i_y != p1_y and i_y != p2_y and i_y != p3_y):
+                    return True
+                else:
+                    return False
+            else:
+                return False
+        else:
+            return False
+
+    except ZeroDivisionError:
         return True
-    else:
-        return False
 
 # Compute the Euclidian distance of a line segment
 def distance(line1):
@@ -67,6 +80,7 @@ def main():
         x_pt = random.random()
         y_pt = random.random()
         next_pt = Point(x_pt * win_sz, y_pt * win_sz)
+        next_pt.setFill('white')
         next_pt.setOutline('white')
         next_pt.draw(win)
         vertices.append(next_pt)
@@ -74,18 +88,19 @@ def main():
     # Create list of all edges between vertices and store in dictionary
     for i in range(0,N):
         edges = []  # Save all edges from each vertex for storage in dictionary
-        for j in range(i+1,N):
-            ln = Line(vertices[i], vertices[j])
-            ln.setFill('red')
-            edge = graph_edge(ln, distance(ln), False)
-            edges.append(edge)
+        for j in range(0,N):
+            if j != i:
+                ln = Line(vertices[i], vertices[j])
+                ln.setFill('red')
+                edge = graph_edge(ln, distance(ln), False)
+                edges.append(edge)
 
         edges.sort(key=lambda x: x.distance)
         fullcon_graph[vertices[i]] = edges      # Insert into dictionary
 
     # Implement graph constructing algorithm defined in problem statement
     escape = True
-    vertex_escape = [False] * (N - 1)
+    vertex_escape = [False] * N     # All elements must be true to escape loop
     while(escape):
         i = random.randint(0,N-1) # Select random point
         edges = fullcon_graph[vertices[i]]      # Select all edges from random point
