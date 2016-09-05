ff6b9cff7ed251a0ef1365145e7549e000c607ac
Roy Smart <roytsmart@gmail.com>
2016-09-05 11:28:15 -0600

    Almost done with problem generator

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # CSCI446_Artificial_Intelligence_Project_1
 We will be attempting to graph coloring using the following algorithms: Min-conflicts, Backtracking, Backtracking with Forward Checking, Backtracking with MAC, and genetic algorithms. We will also write a problem generator to test these algorithms.
+
+To run this program, you need to install the dependency 'Tkinter'. On Ubuntu this can be done by 'sudo apt-get install python3-tk'
---
+++ src/problem_gen.py
@@ -0,0 +1,137 @@
+from graphics import *
+from operator import itemgetter
+import random
+import math
+
+class graph_edge():
+    def __init__(self, ln, distance, if_checked):
+        self.ln = ln
+        self.distance = distance
+        self.checked = if_checked
+
+# Definition of 2D cross product
+
+
+# Check if lines cross by checking if both x and y coordinates simultaneously switch order
+# Solution given by http://stackoverflow.com/a/1968345
+def does_cross(line1, line2):
+
+    p0_x = line1.getP1().getX()
+    p0_y = line1.getP1().getY()
+    p1_x = line1.getP2().getX()
+    p1_y = line1.getP2().getY()
+    p2_x = line2.getP1().getX()
+    p2_y = line2.getP1().getY()
+    p3_x = line2.getP2().getX()
+    p3_y = line2.getP2().getY()
+
+    s1_x = p1_x - p0_x
+    s1_y = p1_y - p0_y
+    s2_x = p3_x - p2_x
+    s2_y = p3_y - p2_y
+
+    s = (-s1_y * (p0_x - p2_x) + s1_x * (p0_y - p2_y)) / (-s2_x * s1_y + s1_x * s2_y)
+    t = (s2_x * (p0_y - p2_y) - s2_y * (p0_x - p2_x)) / (-s2_x * s1_y + s1_x * s2_y)
+
+    print(s,t)
+    if (s > 0 and s < 1 and t > 0 and t < 1):
+        return True
+    else:
+        return False
+
+# Compute the Euclidian distance of a line segment
+def distance(line1):
+    x_dist = line1.getP2().getX() - line1.getP1().getX()
+    y_dist = line1.getP2().getY() - line1.getP1().getY()
+
+    return math.sqrt(x_dist * x_dist + y_dist * y_dist)
+
+
+
+def main():
+
+    # Open graphics window
+    win_sz = 720
+    win = GraphWin('Graph Coloring', win_sz, win_sz)
+    win.setBackground('black')
+
+
+    N = 10      # Number of points provided by command line (not yet)
+    vertices = []    # List of vertices of the graph
+    fullcon_graph = {}      # Dictionary of vertices and all possible edges
+    graph = {}
+    all_edges = []      #
+
+    # Fill the list of points
+    for i in range(0,N):
+        x_pt = random.random()
+        y_pt = random.random()
+        next_pt = Point(x_pt * win_sz, y_pt * win_sz)
+        next_pt.setOutline('white')
+        next_pt.draw(win)
+        vertices.append(next_pt)
+
+    # Create list of all edges between vertices and store in dictionary
+    for i in range(0,N):
+        edges = []  # Save all edges from each vertex for storage in dictionary
+        for j in range(i+1,N):
+            ln = Line(vertices[i], vertices[j])
+            ln.setFill('red')
+            edge = graph_edge(ln, distance(ln), False)
+            edges.append(edge)
+
+        edges.sort(key=lambda x: x.distance)
+        fullcon_graph[vertices[i]] = edges      # Insert into dictionary
+
+    # Implement graph constructing algorithm defined in problem statement
+    escape = True
+    vertex_escape = [False] * (N - 1)
+    while(escape):
+        i = random.randint(0,N-1) # Select random point
+        edges = fullcon_graph[vertices[i]]      # Select all edges from random point
+        num_edges = len(edges)
+
+        # Find next closest point
+        for j in range(0, num_edges):
+
+            test_edge = edges[j]
+
+
+            if test_edge.checked == False:  # Proceed only if we haven't checked this edge
+                test_edge.checked = True  # Make sure to inform the program we checked the edge
+
+                # Check that the test edge does not cross any current edges
+                if len(all_edges) == 0:
+                    graph[vertices[i]] = test_edge
+                    all_edges.append(test_edge)
+                    test_edge.ln.setFill('green')
+                    test_edge.ln.draw(win)
+                    break
+                else:
+                    crosses = False
+                    for edge in all_edges:
+                        if does_cross(test_edge.ln, edge.ln):
+                            crosses = True
+
+                    if crosses == False:
+                        graph[vertices[i]] = test_edge
+                        test_edge.ln.setFill('green')
+                        test_edge.ln.draw(win)
+                        all_edges.append(test_edge)
+                        break
+
+
+
+            # Escape based on full trips through all FOR loops
+            if j == num_edges - 1:
+                vertex_escape[i] = True
+
+        if all(item == True for item in vertex_escape):
+            escape = False
+
+    win.wait_window()
+
+
+
+main()
+
