c2b018be4e1007d4259776acfd955199a6758ecb
Roy Smart <roytsmart@gmail.com>
2016-09-18 21:06:20 -0600

    Added extra file

---
+++ cpp_src/map_problem_gen.cpp
@@ -0,0 +1,5 @@
+//
+// Created by byrdie on 9/18/16.
+//
+
+#include "map_problem_gen.h"
---
+++ cpp_src/map_problem_gen.h
@@ -0,0 +1,8 @@
+//
+// Created by byrdie on 9/18/16.
+//
+
+#ifndef CPP_SRC_MAP_PROBLEM_GEN_H
+#define CPP_SRC_MAP_PROBLEM_GEN_H
+
+#endif //CPP_SRC_MAP_PROBLEM_GEN_H
