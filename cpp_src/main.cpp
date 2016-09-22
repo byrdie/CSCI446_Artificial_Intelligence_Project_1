1549c8a2c98128e10129197e3f26dc717f023de7
nevin <nevin@mint>
2016-09-22 17:30:45 -0600

    Final c-genetic ready for merge.

--- cpp_src/main.cpp
+++ cpp_src/main.cpp
@@ -32,32 +32,22 @@ int main(int argc, char** argv) {
    //ga options
     const int pop_size = 1000;
     const int mut_rate = 30;
-    const int N = 100;
+    const int N = 4;
     init_rand();
 
     Cairo * cairo = new Cairo();
 
     Map * map = problem_gen(N, WIDTH, cairo);
 
+ 
     map->clean_map();
-
+    
+    GeneticAlgorithm * ga = new GeneticAlgorithm(map, pop_size, mut_rate, N, 3);
+    
     auto t1 = chrono::high_resolution_clock::now();
-    backtrack(map, 4, 0);
+    int num_runs=ga->run();
     auto t2 = chrono::high_resolution_clock::now();
     
-            
-    cout << "Number of reads: " << (float) map->num_reads << endl;
-    cout << "Reads/s: " << (float) map->num_reads / chrono::duration_cast<chrono::microseconds>(t2-t1).count() * 1e6 << endl;
-    cout << "Number of writes: " << (float) map->num_writes << endl;
-    cout << "Writes/s: " << (float) map->num_writes / chrono::duration_cast<chrono::microseconds>(t2-t1).count() * 1e6 << endl;
-    
-    
-    map->clean_map();
-    GeneticAlgorithm * ga = new GeneticAlgorithm(map, pop_size, mut_rate, N, 4);
-    
-    t1 = chrono::high_resolution_clock::now();
-    int num_runs=ga->run();
-    t2 = chrono::high_resolution_clock::now();
     printf("\n\n");
     cout << "Number of reads: " << (float) map->num_reads << endl;
     cout << "Reads/s: " << (float) map->num_reads / chrono::duration_cast<chrono::microseconds>(t2-t1).count() * 1e6 << endl;
